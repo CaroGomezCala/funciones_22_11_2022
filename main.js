16cb7ca26b4ed963ada3ea9b4871928c90e49ec4
CaroGomezCala <lcgk.021@gmail.com>
2022-11-28 16:51:16 -0500

    funcion_anonima

--- main.js
+++ main.js
@@ -1,14 +1,10 @@
 addEventListener("DOMContentLoaded", (e) =>{
-    //funcion por expresion: La funcion se guarda dentro de una variable, se declara con la palabra reservada const
-    //Se omite nombrar la funcion ya que seria redundante
-    const saludar = function(){
-        return "Hola!! Soy una funcion por expresion";
-    }
-    console.log(saludar());
 
-    //Continuamos con el ejemplo suma con funcion por expresion
+    //Continuamos con el ejemplo suma con funcion anonima
+    //Al omitir el nombre de la funcion, esta toma el nombre de la variable
+    //A esto se le llama funcion Anonima
     const suma = function(num1,num2){
         console.log(`La suma de los numeros es: ${num1+num2}`);
-    }
+    } 
     suma(7,35);
 })
