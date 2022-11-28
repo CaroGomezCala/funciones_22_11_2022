3f0d5470d6c3a5e9d108ef7274c3f3617c7b7f1d
CaroGomezCala <lcgk.021@gmail.com>
2022-11-28 16:44:43 -0500

    funcion_por_expresion

--- main.js
+++ main.js
@@ -1,19 +1,14 @@
 addEventListener("DOMContentLoaded", (e) =>{
-    //funcion declarada: Esta funcion existira durante todo el codigo, JS procesará, y podremos llamarla en una linea antes de su creacion en el codigo.
-    console.log(typeof saludar);
-
-    //se declara la funcion con la palabra reservada function seguida del nombre de la funcion, de la siguiente manera:
-    function saludar() {
-        return "Hola! Este es un ejemplo de funcion declarada";
+    //funcion por expresion: La funcion se guarda dentro de una variable, se declara con la palabra reservada const
+    //Se omite nombrar la funcion ya que seria redundante
+    const saludar = function(){
+        return "Hola!! Soy una funcion por expresion";
     }
     console.log(saludar());
 
-    //Si requerimos ingresar algun parametro, se haran dentro de los parentesis() separados por una coma
-
-    function suma(num1,num2) {
-        return `La siguiente es la funcion suma que da por resultado: ${num1 + num2}`;
+    //Continuamos con el ejemplo suma con funcion por expresion
+    const suma = function(num1,num2){
+        console.log(`La suma de los numeros es: ${num1+num2}`);
     }
-
-    //al llamar la funcion se debe digitar dentro de los parentesis los valores de los parametros solicitados
-    console.log(suma(5,27));
+    suma(7,35);
 })
