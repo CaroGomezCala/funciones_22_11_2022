76d601fedd5fc23434c8cbf4a7d4498c546116f3
CaroGomezCala <lcgk.021@gmail.com>
2022-11-28 17:09:00 -0500

    funciones_callback

--- main.js
+++ main.js
@@ -1,8 +1,18 @@
 addEventListener("DOMContentLoaded", (e) =>{
 
-    //Funciones como objetos
-    //Las funciones es pueden declarar como si fuesen objetos, aunque no es utilizado normalmente de esta forma
-    //Para esto se guarda en una variable declarada, utilizamos la palabra reservada new seguida de Function
-    const saludar = new Function("return'Hola!! Soy una funcion como objeto';");
-    console.log(saludar());
+    //Funciones Callback
+    //Consiste en crear una funcion B que sera llamada, tomandose como parametro para una funcion A
+    //Debemos definir la funcion B fB
+    const fB = function () {
+        console.log("Función B ejecutada.");
+    };
+
+    //Aqui definimos la funcion A, dentro de los parentesis debe ir la palabra callback
+    //En la estructura interna de la funcion A utilizando el callback se llamara la funcion parametro
+    //De manera que es ahi donde se realiza el llamado a ejecucion de la funcion B.
+    const fA = function (callback) {
+        callback();
+    };
+  
+    fA(fB);
 })
